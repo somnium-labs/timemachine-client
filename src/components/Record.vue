0aafabc9a39f37f10f302e666d14d7eb0e02a63e
김동환 <acepm83@gmail.com>
2019-05-02 08:48:57 +0900

    레코드 출력 포맷 변경

--- src/components/Record.vue
+++ src/components/Record.vue
@@ -21,21 +21,21 @@
                     field="totalBalance"
                     headerText="Total Balance"
                     textAlign="left"
-                    format="#,##"
+                    format="¥#,##"
                 ></e-column>
-                <e-column field="balance" headerText="Balance" textAlign="left" format="#,##"></e-column>
+                <e-column field="balance" headerText="Balance" textAlign="left" format="¥#,##.#"></e-column>
                 <e-column
                     field="ratingBalance"
                     headerText="Rating Balance"
                     textAlign="left"
-                    format="#,##"
+                    format="¥#,##"
                 ></e-column>
-                <e-column field="return" headerText="Returns" textAlign="left" format="#,##"></e-column>
+                <e-column field="return" headerText="Returns" textAlign="left" format="¥#,##"></e-column>
                 <e-column
                     field="cumulativeReturn"
                     headerText="Cumulative Return"
                     textAlign="left"
-                    format="#,##"
+                    format="¥#,##"
                 ></e-column>
                 <e-column field="returnRatio" headerText="Returns(%)" textAlign="left" format="P2"></e-column>
                 <e-column
@@ -48,10 +48,10 @@
                     field="volatilityRatio"
                     headerText="Volatility"
                     textAlign="left"
-                    format="#,##.#"
+                    format="P2"
                 ></e-column>
-                <e-column field="mdd" headerText="MDD" textAlign="left" format="#,##.#"></e-column>
-                <e-column field="max" headerText="Max" textAlign="left" format="#,##"></e-column>
+                <e-column field="mdd" headerText="MDD" textAlign="left" format="P2"></e-column>
+                <e-column field="max" headerText="Max" textAlign="left" format="¥#,##"></e-column>
             </e-columns>
         </ejs-grid>
     </div>
